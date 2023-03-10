1312fd5f7b63a76c003ac53b6644200486415c9d
rohitkhullar786@gmail.com <rohitkhullar786@gmail.com>
2023-03-10 17:15:37 +1100

    changes: main.py

--- main.py
+++ main.py
@@ -1,50 +1,80 @@
-import gradio as gr
-import openai, config, subprocess
-# from gtts import gTTS
-# import os
-openai.api_key = config.OPENAI_API_KEY
+import openai
+import pyttsx3
+import speech_recognition as sr
+import time
 
-messages=[
-        # {"role": "system", "content": "You are a microsoft professional"},
-    ]
+r=sr.Recognizer()
+r.energy_threshold=4000
 
-def transcribe(audio):
-    print(audio)
-    global messages
+#set the api key
+openai.api_key = "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"
 
-    audio_file = open(audio, "rb")
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    print(transcript)
-    
-    messages.append({"role": "user", "content": transcript["text"]})
+# r=sr.Recognizer()
+# r.energy_threshold=4000
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", 
-        messages=messages
+# Initialize the text to speech engine
+
+engine = pyttsx3.init()
+def transcribe_audio_to_text(filename):
+    # recognizer = sr.Recognizer()
+    with sr.Audiofile(filename) as source:
+        audio = r.record(source)
+    try:
+        return r.recognize(audio)
         
-        )
-    
-    print(response)
+    except:
+        print('Skiping unkown error')
 
-    language = 'en'
+def generate_response(prompt):
+    response =openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=4000,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response["choices"][0]["text"]
 
-    system_message = response["choices"][0]["message"]["content"]
-    # output = gTTS(text=system_message, lang=language, slow=False)
-    # output.save("output.mp3")
-    # os.system("start output.mp3")
-    # subprocess.call("say", system_message)
+def speak_text(text):
+    engine.say(text)
+    engine.runandWait()
 
-    messages.append({"role": "assistant", "content": system_message})
+def main():
+    while True:
+        #wait for user to say "genius"
+        print("Say 'Genius' to start recording your question.")
+        with sr.Microphone() as source:
+            recognizer = sr.Recognizer()
+            audio = recognizer.listen(source)
+            try:
+                transciption = recognizer.recognize(audio)
+                
+                if transciption.lower() == "genius":
+                    #Record audio
+                    filename = "input.wav"
+                    print("Say your question")
+                    with sr.Microphone() as source:
+                        recognizer = sr.Recognizer()
+                        source.pause_threshhold = 1
+                        audio = r.listen(source, timeout=None)
+                        with open(filename, "wb") as f:
+                            f.write(audio.get_wav_data())
 
-    # subprocess.call(["say", system_message['content']])
+                    #Transcrive audio to text
+                    text = transcribe_audio_to_text(filename)
+                    if text:
+                        print(f"You said: {text}") 
 
-    chat_transcript = ""
-    for message in messages:
-        if message['role'] != 'system':
-            chat_transcript += message['role'] + ": " + message['content'] + "\n\n"
-    
-    
-    return chat_transcript
+                        #Generate response using chat GPT-3
+                        response = generate_response(text)
+                        print(f"GPT-3 says: {response}")
 
-ui = gr.Interface(fn=transcribe, inputs=gr.Audio(source="microphone", type="filepath"), outputs="text").launch()
-ui.launch()
+                        #Read response using text-to-speech       
+                        speak_text(response)
+
+            except Exception as e:
+                print("An error has occurred: {}".format(e))            
+
+if __name__ == "__main__":
+    main()                
